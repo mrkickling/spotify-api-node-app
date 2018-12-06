3f1d5dd8e22583687151daea5c72aea7d1983438
Joakim Loxdal <loxdal@kth.se>
2018-12-06 17:10:23 +0100

    Better auth

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // Imports
+var helperFunctions = require("./helperFunctions.js");
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
 const querystring = require('querystring');
+var session = require('express-session');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  cookie: { maxAge: false },
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(express.static('public'));
 app.get("/", function (request, response) {
@@ -12,7 +21,7 @@ app.get("/", function (request, response) {
 
 // Initialize Spotify API wrapper
 var SpotifyWebApi = require('spotify-web-api-node');
-var redirect_uri = "http://localhost:3000/callback"
+var REDIRECT_URI = "http://localhost:3000/callback";
 // The object we'll use to interact with the API
 var spotifyApi = new SpotifyWebApi({
   clientId : process.env.CLIENT_ID,
@@ -40,13 +49,53 @@ app.get('/get_access', function (request, response) {
         response_type: 'code',
         client_id: process.env.CLIENT_ID,
         scope: scope,
-        redirect_uri: redirect_uri,
+        redirect_uri: REDIRECT_URI,
         //state: state
       }));
 });
 
-app.get('/callback', function (request, response) {
-  response.send(response);
+app.get('/auth_callback', function (request, response) {
+  request.session.code = request.query.code;
+  response.send(request.query.code);
+
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: authToken,
+      redirect_uri: "https://music-timeline.glitch.me/tracking",
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      var refresh_token = body.refresh_token;
+
+      spotifyApi.setAccessToken(access_token);
+      spotifyApi.setRefreshToken(refresh_token);
+    }
+    else{
+      console.log("error: " + error);
+    }
+  });
+
+});
+
+app.get('/token_callback', function (request, response) {
+
+}
+
+app.get('/my_top_tracks', function (request, response) {
+
+}
+
+app.get('/get_code', function (request, response) {
+  response.send(request.session.code);
 });
 
 var listener = app.listen(port, function(){
