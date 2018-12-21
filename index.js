5583e748ed12d02bad9cfc2b162fc8cb92f69451
Joakim Loxdal <loxdal@kth.se>
2018-12-21 23:19:35 +0100

    Fix problem with https redirect

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ if(process.env.HOST != "http://localhost:3000"){
   function requireHTTPS(req, res, next) {
     // The 'x-forwarded-proto' check is for Heroku
     if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-      return res.redirect('https://' + req.get('host') + req.url);
+      return res.redirect('https://' + process.env.HOST + req.url);
     }
     next();
   }
